b1bee6c93cb5765dc2b311c4c21e9948ccd206d3
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-15 05:28:16 +0100

    Refactor API_URL for Vite environment
    Updated API_URL to use Vite environment variable for compatibility.

--- client/src/services/videoService.js
+++ client/src/services/videoService.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'env.API_URL/api/videos/';
+const API_URL = import.meta.env.VITE_API_URL + '/videos/'; // for Vite
+// or process.env.REACT_APP_API_URL + '/videos/' for CRA
 
 export const getVideos = async () => {
   const res = await axios.get(API_URL);
-  return res.data;
+  return res.data; // make sure this is an array if you plan to .map()
 };
 
 export const getVideoById = async (id) => {
