dc3610339c5ada8849660abd05f20a4ad7e3c9d5
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-17 02:07:11 +0100

    Refactor VideoPage to display loading message
    Removed video fetching logic and replaced it with a simple loading message.

--- client/src/components/VideoPage.js
+++ client/src/components/VideoPage.js
@@ -1,34 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { getVideos } from '../services/videoService';
-import VideoCard from './VideoCard';
-
 const VideoPage = () => {
-  const [videos, setVideos] = useState([]);
-
-  const refreshVideos = async () => {
-    try {
-      const data = await getVideos();
-      setVideos(data);
-    } catch (err) {
-      console.error('Error refreshing videos:', err);
-    }
-  };
-
-  useEffect(() => {
-    refreshVideos();
-  }, []);
-
-  return (
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {videos.map((video) => (
-        <VideoCard
-          key={video._id}
-          video={video}
-          refreshVideos={refreshVideos}
-        />
-      ))}
-    </div>
-  );
+  return <h2>Video Page Loaded</h2>;
 };
 
 export default VideoPage;
