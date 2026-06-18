de8a3f3bfd2a6a60ab3648d8c4f92165c88287ef
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-18 07:39:58 +0100

    Enhance registerUser and remove unused functions
    Updated registerUser to include bio, location, and contacts fields. Removed loginUser and generateToken functions.

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -3,9 +3,8 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// Register new user
 const registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, bio, location, contacts } = req.body;
 
   try {
     const userExists = await User.findOne({ email });
@@ -14,43 +13,25 @@ const registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(password, salt);
 
-    const user = await User.create({ username, email, passwordHash });
+    const user = await User.create({
+      username,
+      email,
+      passwordHash,
+      bio: bio || '',
+      location: location || '',
+      contacts: contacts || {}, // expects { email, phone, linkedin, github, whatsapp }
+    });
 
     res.status(201).json({
       _id: user._id,
       username: user.username,
       email: user.email,
+      bio: user.bio,
+      location: user.location,
+      contacts: user.contacts,
       token: generateToken(user._id),
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
-
-// Login user
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (user && (await bcrypt.compare(password, user.passwordHash))) {
-      res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// Generate JWT
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
-};
-
-module.exports = { registerUser, loginUser };
