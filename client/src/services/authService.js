d7e6a1bb4f79292f1639c7c0175a9320bcd6b2a9
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-06-18 08:24:05 +0100

    Rename register and login functions for clarity

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -6,12 +6,17 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL + '/auth/';
 
-export const register = async (userData) => {
+// Register user
+export const registerUser = async (userData) => {
   const res = await axios.post(API_URL + 'register', userData);
+  if (res.data.token) {
+    localStorage.setItem('token', res.data.token);
+  }
   return res.data;
 };
 
-export const login = async (userData) => {
+// Login user
+export const loginUser = async (userData) => {
   const res = await axios.post(API_URL + 'login', userData);
   if (res.data.token) {
     localStorage.setItem('token', res.data.token);
@@ -19,6 +24,7 @@ export const login = async (userData) => {
   return res.data;
 };
 
+// Logout user
 export const logout = () => {
   localStorage.removeItem('token');
 };
